c477eba13afcba557b4c34ab26b3b3161bd51bc3
vishal <krishnavishal3699@gmail.com>
2021-02-25 09:19:16 +0530

    setting up view engine

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 8000;
 
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 
 app.listen(port,function(err){
     if(err){
@@ -11,5 +14,6 @@ app.listen(port,function(err){
     }
     console.log('Server is running on port',port);
 });
+
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-console.log('router is loaded')
-;router.get('/',homeController.home);
+const userController = require('../controllers/user_controller');
+console.log('router is loaded');
+router.get('/',homeController.home);
+router.use('/user',userController.profile);
 
 
 
