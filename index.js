68eee711832eff20c2e4c6ebf8524d37370c1787
vishal <krishnavishal3699@gmail.com>
2021-02-25 11:43:49 +0530

    Implement layouts

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('view engine','ejs');
+app.use(expressLayouts);
 
 app.use('/',require('./routes'));
 
-app.set('view engine','ejs');
+
 app.set('views','./views');
+
+
 
 
 app.listen(port,function(err){
