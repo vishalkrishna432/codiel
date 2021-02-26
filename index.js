fb29cfacdc3abc833d5f17c9eaf3cdc99f515918
vishal <krishnavishal3699@gmail.com>
2021-02-26 16:34:13 +0530

    setting static file and static file for pages

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-
-app.set('view engine','ejs');
+app.use(express.static('./assets'));
 app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+
 
 app.use('/',require('./routes'));
 
-
+app.set('view engine','ejs');
 app.set('views','./views');
 
 
