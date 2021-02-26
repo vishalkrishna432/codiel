5e67305c35a7e6ec9e16ce4a8d14e2d4d8bd5a86
vishal <krishnavishal3699@gmail.com>
2021-02-26 22:56:53 +0530

    setting us mongodb mongoose robo 3t

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
+const db = require('./config/mongoose');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+
+
 app.use(express.static('./assets'));
 app.use(expressLayouts);
 
