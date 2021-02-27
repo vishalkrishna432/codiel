0e7fe78a28a0be0987886c0f4aa8d1159ffcfb92
vishal <krishnavishal3699@gmail.com>
2021-02-27 15:46:00 +0530

    mannual authentication

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const db = require('./config/mongoose');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
 
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+
+
 
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
-const express = require('express')
+const express = require('express');
+
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-const userController = require('../controllers/user_controller');
-console.log('router is loaded');
-router.get('/',homeController.home);
-router.use('/user',userController.profile);
 
+console.log('router loaded');
+
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
 
 
 module.exports = router;
